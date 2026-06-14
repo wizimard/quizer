627b4b39f673e32d257cd3f6129f9264f5618ada
Maxim Voronin <voronin.m@introvert.bz>
2026-06-14 20:07:03 +0600

    styles(quiz question): border error color if not valid

--- client/src/features/quiz/ui/QuizEditForm/ui/QuestionEdit/QuestionEdit.tsx
+++ client/src/features/quiz/ui/QuizEditForm/ui/QuestionEdit/QuestionEdit.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { FormSelectField, FormTextField } from "@shared/ui/form";
-import { type Control, type UseFieldArrayRemove, useWatch } from "react-hook-form";
+import { type Control, type UseFieldArrayRemove, useWatch, useController } from "react-hook-form";
 import { Text } from "@shared/ui/text";
 import type { IEditQuiz } from "@features/quiz/model/editQuizForm";
 import { QuestionEditConfig } from "./ui/QuestionEditConfig";
@@ -29,6 +29,10 @@ export type TSelectChangeEvent =
 export const QuestionEdit = ({ control, index, isLast, removeQuestion, changeQuestionType, changeQuestionOrder }: IQuestionEditProps) => {
 	const config = useWatch({ name: `questions.${index}.config`, control });
 
+	const {
+		fieldState: { error },
+	} = useController({ control, name: `questions.${index}` });
+
 	const handleChange = (ev: TSelectChangeEvent) => {
 		const type: string = ev.target.value as string;
 
@@ -39,7 +43,7 @@ export const QuestionEdit = ({ control, index, isLast, removeQuestion, changeQue
 			<Text component="span" sx={{ paddingTop: "3px" }}>
 				{index + 1}
 			</Text>
-			<QuestionListItemContainer>
+			<QuestionListItemContainer isError={!!error}>
 				<QuestionEditHeader index={index} isLast={isLast} removeQuestion={removeQuestion} changeQuestionOrder={changeQuestionOrder} />
 				<FormTextField control={control} name={`questions.${index}.description`} placeholder="Введите описание задачи" label="Описание задачи" sx={{ marginBottom: "20px" }} />
 				<FormSelectField onChange={handleChange} control={control} name={`questions.${index}.config.type`} id={`questions.${index}.type`} options={QUESTION_TYPES_OPTIONS} label="Тип ответа" />
