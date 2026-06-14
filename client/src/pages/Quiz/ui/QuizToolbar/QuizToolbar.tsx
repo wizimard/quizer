d629bc3f34a3049f62d592fb1683a2b12296829e
Maxim Voronin <voronin.m@introvert.bz>
2026-06-14 20:46:35 +0600

    refactoring

--- client/src/pages/Quiz/ui/QuizToolbar/QuizToolbar.tsx
+++ client/src/pages/Quiz/ui/QuizToolbar/QuizToolbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, ButtonGroup, Dialog, DialogActions, DialogTitle } from "@mui/material";
+import { Box, ButtonGroup } from "@mui/material";
 import type { QuizResponse } from "@shared/api/generated";
 import { ButtonWithIcon } from "@shared/ui/button";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
@@ -6,17 +6,16 @@ import EditIcon from "@mui/icons-material/Edit";
 import { Text } from "@shared/ui/text";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import { api } from "@shared/api";
+import { QuizDeleteDialog } from "./QuizDeleteDialog";
 
 export interface IQuizToolbar {
 	quiz: QuizResponse;
 }
 
 export const QuizToolbar = ({ quiz: { id, title } }: IQuizToolbar) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
-	const [isLoading, setIsLoading] = useState<boolean>(false);
-
 	const navigate = useNavigate();
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	const handleClickEdit = () => {
 		navigate(`/quiz-edit/${id}`);
@@ -30,20 +29,6 @@ export const QuizToolbar = ({ quiz: { id, title } }: IQuizToolbar) => {
 		setIsOpen(false);
 	};
 
-	const handleRemove = async () => {
-		setIsLoading(true);
-
-		try {
-			await api.quizIdDelete(id);
-
-			navigate("/");
-		} catch (err) {
-			console.error(err);
-		}
-
-		setIsLoading(false);
-	};
-
 	return (
 		<>
 			<Box sx={{ width: "100%", display: "flex", justifyContent: "space-between" }}>
@@ -55,17 +40,7 @@ export const QuizToolbar = ({ quiz: { id, title } }: IQuizToolbar) => {
 					<ButtonWithIcon IconComponent={DeleteForeverIcon} text="Удалить" color="error" onClick={handleClickRemove} />
 				</ButtonGroup>
 			</Box>
-			<Dialog open={isOpen} onClose={handleCloseDialog}>
-				<DialogTitle>Вы уверены, что хотите удалить викторину?</DialogTitle>
-				<DialogActions>
-					<Button autoFocus onClick={handleCloseDialog}>
-						Отменить
-					</Button>
-					<Button color="error" onClick={handleRemove} loading={isLoading}>
-						Удалить
-					</Button>
-				</DialogActions>
-			</Dialog>
+			<QuizDeleteDialog quizId="id" handleClose={handleCloseDialog} isOpen={isOpen} />
 		</>
 	);
 };
