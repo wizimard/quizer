27223b1ffef687ae95084120e9004049da56730f
wizimard <wizaxmardim@gmail.com>
2026-06-30 22:14:50 +0600

    migrate: remove quiz isOpen flag

---
+++ server/prisma/migrations/20260630161417_quiz_remove_is_open_flag/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isOpen` on the `QuizModel` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "QuizModel" DROP COLUMN "isOpen";
